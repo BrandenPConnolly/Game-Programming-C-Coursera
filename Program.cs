47046bdb02ae5e7d1362ae37ba7fca73716d3f2e
Branden Connolly <Brandenpconnolly@gmail.com>
2017-08-26 21:39:37 -0500

    First C# program
    Basic Hello World

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstCSharpProgram
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine();
+        }
+    }
+}

b1b1243472f97b96e00a16076c9f43c6b7f80d01
Branden Connolly <Brandenpconnolly@gmail.com>
2017-09-04 18:09:05 -0500

    More Work with Numbers

---
+++ MoreNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WeekOneLabTwo
+{
+    /// <summary>
+    /// Lab Number 2 - Variables and Constants
+    /// </summary>
+    class MainClass
+    {
+        /// <summary>
+        /// Variables and Constants
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            //Define Variables and Constants
+            //int myAge = 29;
+            const int MaxScore = 100;
+            int score = 67;
+            float percent = score / (float)MaxScore;
+
+            //Outputs
+            //Console.WriteLine("My age is: " + myAge);
+            Console.WriteLine("Score Percentage: " + percent);
+
+
+        }
+    }
+}

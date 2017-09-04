45862e11db5a21cb80156a808fa38648be5ce803
Branden Connolly <Brandenpconnolly@gmail.com>
2017-09-04 17:19:17 -0500

    Dealing with floating point numbers

---
+++ FloatingPointNums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FloatingPointNums
+{
+    /// <summary>
+    /// Demonstrates Flaoting Point Numbers
+    /// </summary>
+    class MainClass
+    {
+        /// <summary>
+        /// Demonstrates Floating Point Numbers
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            //Declaring variables
+
+            int score = 2546;
+            float totalSecondsPlayed = 6574;
+
+            //calculate and display result
+
+            float scorePerSecond = score / totalSecondsPlayed;
+
+            Console.WriteLine("Score / Second: " + scorePerSecond);
+            Console.WriteLine();
+        }
+    }
+}

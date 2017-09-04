0371062c535da332f48a9458aa5b8274adb7471a
Branden Connolly <Brandenpconnolly@gmail.com>
2017-09-04 18:11:12 -0500

    Very simple game that states if a number is even or odd

---
+++ EvenOddGame.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FirstCSharpProgram
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Even or Odd!");
+
+            int s;
+            string replay;
+            replay = "true";
+            while (replay == "true")
+            {
+
+                Console.WriteLine("Enter a number, any number!");
+                s = Convert.ToInt32(Console.ReadLine());
+
+                if (s % 2 == 0)
+                {
+                    Console.WriteLine("It is an even number!");
+                }
+                else
+                {
+                    Console.WriteLine("It is an odd number!");
+                }
+                Console.WriteLine("Do you want to play again?(true/false)");
+                replay = Console.ReadLine();
+            }
+        }
+    }
+}

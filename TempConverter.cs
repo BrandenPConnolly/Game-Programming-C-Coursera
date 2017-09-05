c3741316a36e1681aee481d7ee649a1742b24987
Branden Connolly <Brandenpconnolly@gmail.com>
2017-09-04 19:10:54 -0500

    Converts F to C or C to F

---
+++ TempConverter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace weekonelabthree
+{
+    class MainClass
+    {
+        public static float FtoC(float value)
+        {
+            //declare variables
+            value = value - 32;
+            value = value / 9;
+            value = value * 5;
+
+            return value;
+        }
+		public static float CtoF(float value)
+		{
+            //declare variables
+            value = value * 1.8f + 32;
+
+			return value;
+		}
+        public static void Main(string[] args)
+        {
+            //Declaring variables
+            bool contProg = true;
+            bool ans1 = true;
+            bool ans2 = false;
+            string origValue = "C";
+            float origTemp;
+            string nextTurn = "Yes";
+
+            //Main loop
+            Console.WriteLine("Welcome to the tempurature translator!");
+            while (contProg == true)
+            {
+                Console.WriteLine("Is original value C or F? (C or F only)");
+                origValue = Console.ReadLine();
+                ans1 = origValue.Equals("C", StringComparison.Ordinal);
+                ans2 = origValue.Equals("F", StringComparison.Ordinal);
+				//result = root.Equals(root2, StringComparison.Ordinal);
+                if (ans1 || ans2)
+                {
+                    Console.WriteLine("What is the starting value?");
+                    origTemp = float.Parse(Console.ReadLine());
+                    if (ans1 == true)
+                    {
+                        Console.WriteLine("Your temp in F: " + CtoF(origTemp));
+                    }
+                    else if (ans2 == true)
+                    {
+                        Console.WriteLine("Your temp in C: " + FtoC(origTemp));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter only F or C");
+                }
+                Console.WriteLine("Do you have another conversion needed? (Yes or No)");
+                nextTurn = Console.ReadLine();
+                if(nextTurn.Equals("Yes", StringComparison.Ordinal))
+                {
+                    contProg = true;
+                }
+                else if(nextTurn.Equals("No", StringComparison.Ordinal))
+                {
+                    contProg = false;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Value...Ending Program");
+                    contProg = false;
+                }
+                Console.WriteLine();
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
